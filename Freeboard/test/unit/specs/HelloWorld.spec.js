8c7b7a16291e19bfa9c2b5058afdb26e24eeaac1
GwangMinBack <bgm8862@gmail.com>
2018-04-19 22:54:47 +0900

    Main페이지 테스트 케이스 작성

--- Freeboard/test/unit/specs/HelloWorld.spec.js
+++ Freeboard/test/unit/specs/HelloWorld.spec.js
@@ -1,11 +1,31 @@
-import Vue from 'vue'
-import HelloWorld from '@/components/HelloWorld'
+import Vue from 'vue';
+import Main from '@/components/Main.vue';
 
-describe('HelloWorld.vue', () => {
-  it('should render correct contents', () => {
-    const Constructor = Vue.extend(HelloWorld)
-    const vm = new Constructor().$mount()
-    expect(vm.$el.querySelector('.hello h1').textContent)
-      .toEqual('Welcome to Your Vue.js App')
-  })
-})
+
+describe('Main.vue 테스트', () => {
+  it('"No"가 정상적으로 들어갔나요?', () => {
+    const Constructor = Vue.extend(Main);
+    const vm = new Constructor().$mount();
+    expect(vm.$el.querySelector('.Main .post-list .post-header .post-header__index').textContent)
+      .toEqual('No');
+  });
+});
+
+describe('Main.vue 테스트', () => {
+  it('"제목"이 정상적으로 들어갔나요?', () => {
+    const Constructor = Vue.extend(Main);
+    const vm = new Constructor().$mount();
+    expect(vm.$el.querySelector('.Main .post-list .post-header .post-header__title').textContent)
+      .toEqual('title');
+  });
+});
+
+
+describe('contents.vue 테스트', () => {
+  it('Main.vue가 보내준 값을 잘 받을 수 있나요?', () => {
+    const Constructor = Vue.extend(Main);
+    const vm = new Constructor().$mount();
+    expect(vm.$el.querySelector('.post__index').textContent)
+      .toEqual('1');
+  });
+});
---
+++ freeboard/src/components/Main/contents.vue
@@ -0,0 +1,38 @@
+<template>
+  <div class="post">
+    <div class="post__index" >{{ post.index }}</div>
+    <div class="post__title">{{ post.title }}</div>
+  </div>
+</template>
+
+<script>
+export default {
+  name:"Contents",
+  data(){
+    return{
+    }
+  },
+  props: ['post']
+}
+</script>
+
+<style scoped>
+.post{
+    border-bottom: #000 1px solid;
+    padding-left: 15px;
+
+}
+.post__index, .post__title{
+  display: inline-block;
+  font-size: 20px;
+}
+.post__index{
+  position: relative;
+  width: 30px;
+
+}
+.post__title{
+  margin-left: 50px;
+}
+</style>
+
